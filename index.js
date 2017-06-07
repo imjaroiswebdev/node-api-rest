e4209bc7776e30a5f91bc05e00b9e7c2ab25d2de
José Reyes <imjaroiswebdev@gmail.com>
2017-06-07 07:18:41 -0400

    Funcionando endpoints get y post.
    Get esta funcinando para mostrar la collección completa
    de productos e igualmente para un producto en particular
    según su id. Integraciones basicas de estos metodos.
    Seguimos con put y delete.

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+// Conexión con los esquemas de la base de datos
+const Product = require('./models/product');
+
+
 const app = express();
 
 // Se tendrá una variable port para que funcione tanto
@@ -20,18 +24,56 @@ app.use(bodyParser.json());
 
 // Ruta para petición de productos
 app.get('/api/product', (req, res) => {
-	res.status(200).send({products: []});
+	Product.find({}, (err, products) => {
+		// Para errores internos
+		if (err) return res.status(500).send({message: `Error al tratar de ubicar el producto ${err}`});
+		// Si el producto no existe en la base de datos
+		if (!products) return res.status(404).send({message: 'No hay productos actualmente.'});
+
+		res.status(200).send({ products });
+	});
 });
 
 // Ruta para acceder a productos según ID
 app.get('/api/product/:productId', (req, res) => {
+	let productId = req.params.productId;
 
+	// Se ubica el producto por su ID en la base de datos a través de find()
+	// con el metodo de Mongoose
+	Product.findById(productId, (err, product) => {
+		// Para errores internos
+		if (err) return res.status(500).send({message: `Error al tratar de ubicar el producto ${err}`});
+		// Si el producto no existe en la base de datos
+		if (!product) return res.status(404).send({message: 'El producto no existe.'});
+
+		// Producto ubicado product: product (reducido a solo product, ya que 
+		// el objeto y la clave tienen el mismo nombre)
+		res.status(200).send({ product });
+	});
 });
 
 // Ruta para creación de productos
 app.post('/api/product', (req, res) => {
+	console.log('POST /api/product');
 	console.log(req.body);
-	res.status(200).send({message: 'Producto creado exitosamente.'});
+
+	let product = new Product();
+
+	product.name = req.body.name;
+	product.picture = req.body.picture;
+	product.price = req.body.price;
+	product.category = req.body.category;
+	product.description = req.body.description;
+
+	// Almacenamiento del nuevo producto en la base de datos
+	product.save((err, productStored) => {
+		if (err) res.status(500)
+			.send({
+				message: `Error al intentar guardar en la base de datos: ${err}`
+			});
+
+		res.status(200).send({product: productStored});
+	});
 });
 
 // Ruta para actualización de producto de acceso según su ID
@@ -46,7 +88,7 @@ app.delete('/api/product/:productId', (req, res) => {
 
 // Primeramente conectarse a la base de datos para asegurar que la API no 
 // funcione sin acceso al almacenamiento.
-mongoose.connect('mongodb://localhost/27017/shop', (err, res) => {
+mongoose.connect('mongodb://localhost:27017/shop', (err, res) => {
 	if (err) {
 		return console.log(`Error al intentar conectarse a la base de datos:
 		${err}`)
---
+++ models/product.js
@@ -0,0 +1,16 @@
+'use strict';
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Esquema de Productos para almacenamiento en la base de datos
+
+const ProductSchema = Schema({
+	name: String,
+	picture: String,
+	price: { type: Number, default: 0 },
+	category: { type: String, enum: ['laptops', 'telefonos', 'accesorios'] },
+	description: String
+});
+
+module.exports = mongoose.model('Product', ProductSchema);
