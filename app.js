df15aeaf81e67f979d3d2fbc17161105ad8325d9
José Reyes <imjaroiswebdev@gmail.com>
2017-06-21 07:02:06 -0400

    Vistas de consulta de productos y login implementada.
    Para hacer una prueba del endpoint para inicio de sesión
    y consulta de los productos usando local storage para
    el caso de uso por browser se implementó una ruta para
    login y en la raiz una vez ya se cuenta con un token
    se pueden consultar los productos en la base de datos.

--- app.js
+++ app.js
@@ -3,6 +3,9 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
+// Motor de plantillas para renderizado de vistas
+const hbs = require('express-handlebars');
+
 const app = express();
 
 // Se importan los end points generados por el router
@@ -11,8 +14,25 @@ const api = require('./routes');
 
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(bodyParser.json());
+
+// Configuración  de las plantillas
+app.engine('.hbs', hbs({
+	defaultLayout: 'default',
+	extname: '.hbs'
+}));
+app.set('view engine', '.hbs');
+
+
 // Indicamos a la app que utilice las rutas de los end points generados por api
 app.use('/api', api);
+
+// Renderizados de vistas
+app.get('/login', (req, res) => {
+	res.render('login')
+});
+app.get('/', (req, res) => {
+	res.render('product')
+});
 
 
 module.exports = app;
